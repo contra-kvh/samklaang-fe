5df37d92da1dd4912b8e1c591a66648731edf827
esskayesss <imesskayesss@protonmail.com>
2024-07-29 10:21:00 +0530

    root has some quick links now

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,17 @@
 "use client";
 
 import { Button } from "@/components/ui/button";
-import useAuth from "@/lib/hooks/useAuth";
 
 export default function Home() {
-  const { logout } = useAuth();
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="flex gap-16 justify-center items-center h-fit">
-        <Button text="Authenticate" />
-        <Button text="Register" type="secondary" />
-
-        <Button text="Logout" type="primary" onClick={logout} />
+      <div className="flex flex-col gap-8 justify-center items-center h-fit">
+        <p className="text-xl">Quick Links</p>
+        <div className="flex gap-8 justify-center items-center">
+          <Button type="primary" text="Authenticate" href="/auth/login" />
+          <Button type="primary" text="Dashboard" href="/dashboard" />
+        </div>
+        <Button type="secondary" text="UI Components Showcase" href="/components" />
       </div>
     </main>
   );
